6518269e01a992cba17abb52f0bdc09cd7e436b1
Haiyu <hwu6@nd.edu>
2024-12-20 20:25:56 -0500

    fix wrong code

--- pixel_generator/vec2face/taming/modules/discriminator_loss.py
+++ pixel_generator/vec2face/taming/modules/discriminator_loss.py
@@ -1,114 +1,128 @@
-import torch.nn as nn
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from models import iresnet
+from lpips.lpips import LPIPS
+from pytorch_msssim import SSIM
 
-####################################ViT-VQGAN########################################
-# https://github.com/lucidrains/parti-pytorch/blob/main/parti_pytorch/vit_vqgan.py#L171
-#####################################################################################
-def default(val, d):
-    return val if exists(val) else d
 
-def exists(val):
-    return val is not None
+def adopt_weight(weight, global_step, threshold=0, value=0.):
+    if global_step < threshold:
+        weight = value
+    return weight
 
-def leaky_relu(p = 0.1):
-    return nn.LeakyReLU(0.1)
 
-class CrossEmbedLayer(nn.Module):
-    def __init__(
-        self,
-        dim_in,
-        kernel_sizes,
-        dim_out = None,
-        stride = 2
-    ):
+def hinge_d_loss(logits_real, logits_fake):
+    loss_real = torch.mean(F.relu(1. - logits_real))
+    loss_fake = torch.mean(F.relu(1. + logits_fake))
+    d_loss = 0.5 * (loss_real + loss_fake)
+    return d_loss
+
+
+def mse_d_loss(logits_real, logits_fake):
+    loss_real = torch.mean((logits_real - 1.) ** 2)
+    loss_fake = torch.mean(logits_fake ** 2)
+    d_loss = 0.5 * (loss_real + loss_fake)
+    return d_loss
+
+
+def vanilla_d_loss(logits_real, logits_fake):
+    d_loss = 0.5 * (
+            torch.mean(torch.nn.functional.softplus(-logits_real)) +
+            torch.mean(torch.nn.functional.softplus(logits_fake)))
+    return d_loss
+
+
+def create_fr_model(model_path, depth="100", use_amp=True):
+    model = iresnet(depth)
+    model.load_state_dict(torch.load(model_path))
+    if use_amp:
+        model.half()
+    return model
+
+
+class VQLPIPSWithDiscriminator(nn.Module):
+    def __init__(self, disc_start=1000, disc_factor=1.0, disc_weight=1.0,
+                 disc_conditional=False, disc_loss="mse", id_loss="mse",
+                 fr_model="./weights/arcface-r100-glint360k.pth",
+                 not_use_g_loss_adaptive_weight=False, use_amp=True):
         super().__init__()
-        assert all([*map(lambda t: (t % 2) == (stride % 2), kernel_sizes)])
-        dim_out = default(dim_out, dim_in)
+        assert disc_loss in ["hinge", "vanilla", "mse", "smooth"]
+        self.loss_name = disc_loss
+        self.perceptual_loss = LPIPS().eval()
+        self.discriminator_iter_start = disc_start
+        if disc_loss == "hinge":
+            self.disc_loss = hinge_d_loss
+        elif disc_loss == "vanilla":
+            self.disc_loss = vanilla_d_loss
+        elif disc_loss == "mse":
+            self.disc_loss = mse_d_loss
+        else:
+            raise ValueError(f"Unknown GAN loss '{disc_loss}'.")
+        print(f"VQLPIPSWithDiscriminator running with {disc_loss} loss.")
+        self.fr_model = create_fr_model(fr_model, use_amp=use_amp).eval()
+        if id_loss == "mse":
+            self.feature_loss = nn.MSELoss()
+        elif id_loss == "cosine":
+            self.feature_loss = nn.CosineSimilarity()
+        self.disc_factor = disc_factor
+        self.discriminator_weight = disc_weight
+        self.disc_conditional = disc_conditional
+        self.ssim_loss = SSIM(data_range=1, size_average=True, channel=3)
+        self.not_use_g_loss_adaptive_weight = not_use_g_loss_adaptive_weight
 
-        kernel_sizes = sorted(kernel_sizes)
-        num_scales = len(kernel_sizes)
+    def calculate_adaptive_weight(self, nll_loss, g_loss, last_layer=None):
+        if last_layer is not None:
+            nll_grads = torch.autograd.grad(nll_loss, last_layer, retain_graph=True)[0]
+            g_grads = torch.autograd.grad(g_loss, last_layer, retain_graph=True)[0]
+        else:
+            nll_grads = torch.autograd.grad(nll_loss, self.last_layer[0], retain_graph=True)[0]
+            g_grads = torch.autograd.grad(g_loss, self.last_layer[0], retain_graph=True)[0]
 
-        # calculate the dimension at each scale
-        dim_scales = [int(dim_out / (2 ** i)) for i in range(1, num_scales)]
-        dim_scales = [*dim_scales, dim_out - sum(dim_scales)]
+        d_weight = torch.norm(nll_grads) / (torch.norm(g_grads) + 1e-4)
+        d_weight = torch.clamp(d_weight, 0.0, 1e4).detach()
+        d_weight = d_weight * self.discriminator_weight
+        return d_weight
 
-        self.convs = nn.ModuleList([])
-        for kernel, dim_scale in zip(kernel_sizes, dim_scales):
-            self.convs.append(nn.Conv2d(dim_in, dim_scale, kernel, stride = stride, padding = (kernel - stride) // 2))
+    def forward(self, im_features, gt_indices, logits, gt_img, image, discriminator, emb_loss,
+                epoch, last_layer=None, cond=None, mask=None):
+        rec_loss = (image - gt_img) ** 2
+        if epoch >= 0:
+            gen_feature = self.fr_model(image)
+            feature_loss = torch.mean(1 - torch.cosine_similarity(im_features, gen_feature))
+        else:
+            feature_loss = 0
 
-    def forward(self, x):
-        fmaps = tuple(map(lambda conv: conv(x), self.convs))
-        return torch.cat(fmaps, dim = 1)
+        p_loss = self.perceptual_loss(image, gt_img) * 2
 
-class Block(nn.Module):
-    def __init__(
-        self,
-        dim,
-        dim_out,
-        groups = 8
-    ):
-        super().__init__()
-        self.groupnorm = nn.GroupNorm(groups, dim)
-        self.activation = leaky_relu()
-        self.project = nn.Conv2d(dim, dim_out, 3, padding = 1)
+        with torch.cuda.amp.autocast(enabled=False):
+            ssim_loss = 1 - self.ssim_loss((image.float() + 1) / 2, (gt_img + 1) / 2)
+        logits_fake = discriminator(image)
+        logits_real_d = discriminator(gt_img.detach())
+        logits_fake_d = discriminator(image.detach())
 
-    def forward(self, x, scale_shift = None):
-        x = self.groupnorm(x)
-        x = self.activation(x)
-        return self.project(x)
+        if mask is None:
+            token_loss = (logits[:, 1:, :] - gt_indices[:, 1:, :])
+            token_loss = torch.mean(token_loss ** 2)
+        else:
+            token_loss = torch.abs((logits[:, 1:, :] - gt_indices[:, 1:, :])) * mask[:, 1:, None]
+            token_loss = token_loss.sum() / mask[:, 1:].sum()
 
-class ResnetBlock(nn.Module):
-    def __init__(
-        self,
-        dim,
-        dim_out = None,
-        *,
-        groups = 8
-    ):
-        super().__init__()
-        dim_out = default(dim_out, dim)
-        self.block = Block(dim, dim_out, groups = groups)
-        self.res_conv = nn.Conv2d(dim, dim_out, 1) if dim != dim_out else nn.Identity()
+        nll_loss = torch.mean(rec_loss + p_loss) + \
+                   ssim_loss + \
+                   token_loss + feature_loss + emb_loss
+        # generator update
+        g_loss = -torch.mean(logits_fake)
 
-    def forward(self, x):
-        h = self.block(x)
-        return h + self.res_conv(x)
+        if not self.not_use_g_loss_adaptive_weight or epoch < self.discriminator_iter_start:
+            d_weight = 1.0
+        else:
+            d_weight = self.calculate_adaptive_weight(nll_loss, g_loss, last_layer=last_layer)
+        disc_factor = adopt_weight(self.disc_factor, epoch, threshold=self.discriminator_iter_start)
 
+        ae_loss = nll_loss + d_weight * disc_factor * g_loss
 
-class Discriminator(nn.Module):
-    def __init__(
-        self,
-        dims,
-        channels = 3,
-        groups = 8,
-        init_kernel_size = 5,
-        cross_embed_kernel_sizes = (3, 7, 15)
-    ):
-        super().__init__()
-        init_dim, *_, final_dim = dims
-        dim_pairs = zip(dims[:-1], dims[1:])
-
-        self.layers = nn.ModuleList([nn.Sequential(
-            CrossEmbedLayer(channels, cross_embed_kernel_sizes, init_dim, stride = 1),
-            leaky_relu()
-        )])
-
-        for dim_in, dim_out in dim_pairs:
-            self.layers.append(nn.Sequential(
-                nn.Conv2d(dim_in, dim_out, 4, stride = 2, padding = 1),
-                leaky_relu(),
-                nn.GroupNorm(groups, dim_out),
-                ResnetBlock(dim_out, dim_out),
-            ))
-
-        self.to_logits = nn.Sequential( # return 5 x 5, for PatchGAN-esque training
-            nn.Conv2d(final_dim, final_dim, 1),
-            leaky_relu(),
-            nn.Conv2d(final_dim, 1, 4),
-            nn.LayerNorm(11)
-        )
-
-    def forward(self, x):
-        for net in self.layers:
-            x = net(x)
-        return self.to_logits(x)
+        # second pass for discriminator update
+        disc_factor = adopt_weight(self.disc_factor, epoch, threshold=self.discriminator_iter_start)
+        d_loss = disc_factor * self.disc_loss(logits_real_d, logits_fake_d)
+        return ae_loss, d_loss, token_loss, rec_loss, ssim_loss, p_loss, feature_loss
